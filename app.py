13b5aab84b0996a184f8933042d0e8f4d7a5f336
Isabel Sommerfeld <isabel.k.sommerfeld@gmail.com>
2026-09-03 10:58:15 +0200

    app final url changes

--- app.py
+++ app.py
@@ -40,7 +40,7 @@ sex = st.selectbox("Sex", ["Male", "Female"])
 
 # 2. Button & API-Anfrage
 if st.button("Predict"):
-    url = "http://127.0.0.1:8080/predict"
+    url = f"{api_url}/predict"
 
     params = {
         "island": island,
